08c6d57c24b32f5e4f5e6e80fbca1d4b5ba322f2
Kazutoshi Shinoda <kazutoshi.shinoda0516@gmail.com>
2017-08-19 18:50:10 +0900

    Add path to train

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ _CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_VGR_domain_text = _CUR_DIR + '/data/kokoro0.txt'
 
 PATH_TO_VOCABU = _CUR_DIR + '/data/vocabu/vacabu.txt'
+
+PATH_TO_X_TRAIN = _CUR_DIR + '/data/train/X_train.txt'
+
+PATH_TO_Y_TRAIN = _CUR_DIR + '/data/train/y_train.txt'
