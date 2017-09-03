a82a6eea43f6d6205d4ec19bddfbf040782a73d3
Kazutoshi Shinoda <kazutoshi.shinoda0516@gmail.com>
2017-09-03 14:38:34 +0900

    Add eng files

--- config.py
+++ config.py
@@ -8,28 +8,33 @@ _CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 # ソース
 
 PATH_TO_VGR_domain_text = _CUR_DIR + '/data/VGR_domain_text'
-
 PATH_TO_VGR_domain_text2 = _CUR_DIR + '/data/VGR_domain_text2'
 
 # 単語レベルと文字レベルの語彙
 
 PATH_TO_WORD_VOCAB = _CUR_DIR + '/data/vocab/word_vocab.txt'
-
 PATH_TO_CHAR_VOCAB = _CUR_DIR + '/data/vocab/char_vocab.txt'
-
 PATH_TO_WORD_VOCAB2 = _CUR_DIR + '/data/vocab/word_vocab2.txt'
-
 PATH_TO_CHAR_VOCAB2 = _CUR_DIR + '/data/vocab/char_vocab2.txt'
 
 # 入出力用データ
 
 PATH_TO_X_TRAIN = _CUR_DIR + '/data/train/X_train.txt'
-
 PATH_TO_Y_TRAIN = _CUR_DIR + '/data/train/y_train.txt'
-
 PATH_TO_X_TEST = _CUR_DIR + '/data/test/X_test.txt'
-
 PATH_TO_Y_TEST = _CUR_DIR + '/data/test/y_test.txt'
+
+# 英語データ
+
+PATH_TO_ENG_X_TRAIN = _CUR_DIR + '/data/AESW/aesw_X_train.txt'
+PATH_TO_ENG_Y_TRAIN = _CUR_DIR + '/data/AESW/aesw_y_train.txt'
+PATH_TO_ENG_X_TEST = _CUR_DIR + '/data/AESW/aesw_X_test.txt'
+PATH_TO_ENG_Y_TEST = _CUR_DIR + '/data/AESW/aesw_y_test.txt'
+
+PATH_TO_ENG_SOURCE_WORD_VOCAB = _CUR_DIR + '/data/vocab/eng_source_word_vocab.txt'
+PATH_TO_ENG_TARGET_WORD_VOCAB = _CUR_DIR + '/data/vocab/eng_target_word_vocab.txt'
+PATH_TO_ENG_SOURCE_CHAR_VOCAB = _CUR_DIR + '/data/vocab/eng_source_char_vocab.txt'
+PATH_TO_ENG_TARGET_CHAR_VOCAB = _CUR_DIR + '/data/vocab/eng_target_char_vocab.txt'
 
 # 学習済みモデル
 
